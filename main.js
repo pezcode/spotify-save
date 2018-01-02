e187771d4e6be315713eda4a00ebd3035304beb1
pezcode <mail@rvrs.in>
2018-01-02 20:22:54 +0100

    fix icon selection

--- main.js
+++ main.js
@@ -10,15 +10,15 @@ let tray = null
 
 // Application icons
 // Let Windows pick appropriate sizes from .ico
-let appIcon  = 'assets/icons/' + process.platform === 'win32' ? 'win/icon.ico' : 'png/1024x1024.png';
-let trayIcon = 'assets/icons/' + process.platform === 'win32' ? 'win/icon.ico' : 'png/64x64.png';
+let appIcon  = 'assets/icons/' + (process.platform === 'win32' ? 'win/icon.ico' : 'png/1024x1024.png')
+let trayIcon = 'assets/icons/' + (process.platform === 'win32' ? 'win/icon.ico' : 'png/64x64.png')
 
 function createSettingsWindow (onClosed) {
   // Create the browser window.
   var win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, 'assets/icons/win/icon.ico') // taskbar and handle icon
+    icon: path.join(__dirname, appIcon) // taskbar and handle icon
   })
   // and load the html of the window.
   win.loadURL(url.format({
@@ -43,7 +43,7 @@ function onShowSettings () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', function() {
-  tray = new Tray(path.join(__dirname, 'assets/icons/win/icon.ico'))
+  tray = new Tray(path.join(__dirname, trayIcon))
   tray.setToolTip('spotify-save')
   const contextMenu = Menu.buildFromTemplate([
     {label: 'Settings', type: 'normal', click: onShowSettings},
